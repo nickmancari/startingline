a284e649bb81730b3d075218c5f5f245313a5a72
nickmancari <nickjmancari@gmail.com>
2020-10-14 09:46:13 -0500

    script added

---
+++ General/keys.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##
+###
+####
+#####
+echo What would you like to name the key?
+#
+read KEYNAME
+#
+ssh-keygen -f ~/.ssh/$KEYNAME
+#
+echo Where is this going?
+#
+read SERVER
+#
+echo With what user?
+#
+read USER
+#
+ssh-copy-id -i ~/.ssh/$KEYNAME $USER@$SERVER
+#####
+####
+###
+##
+#
