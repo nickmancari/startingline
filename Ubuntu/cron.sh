a02a3b51023eb71a96de3893f80a72e93823e71b
nickmancari <nickjmancari@gmail.com>
2020-10-16 13:36:28 -0500

    update

--- Ubuntu/cron.sh
+++ Ubuntu/cron.sh
@@ -5,4 +5,4 @@
 # Create cronjob to update sytem everyday and 
 # logs it in a file in the root home folder
 touch ~/.update_log.txt
-
+echo "0 1 * * * apt-get -y update upgrade && tee -a ~/.update_log.txt" | crontab -
