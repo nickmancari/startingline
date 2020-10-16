26a952692e35eeee92041058a0db955c321fb3e9
nickmancari <nickjmancari@gmail.com>
2020-10-16 14:09:40 -0500

    changes to CentOS folder

---
+++ CentOS/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# All reoccuring automated jobs for an Ubuntu System go here
+##############
+##############
+# Create cronjob to update sytem everyday and
+# logs it in a file in the root home folder
+touch ~/.update_log.txt
+echo "0 1 * * * yum -y update &>> ~/.update_log.txt" | crontab -
