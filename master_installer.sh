f360079b3063b3ad753a6b32f12d41eeed270c91
nickmancari <nickjmancari@gmail.com>
2020-10-16 16:16:49 -0500

    fixed comment in master_installer.sh

--- master_installer.sh
+++ master_installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# First, we check which distro is installed to
-# get distro-specific install scripts
+# 
+# 
 # First we establish if the user
 # running the script is root
 # if not they can't run it
