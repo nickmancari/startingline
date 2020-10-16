f7617540c010678ba0ac5e30086b19ab086bf514
nickmancari <nickjmancari@gmail.com>
2020-10-16 10:52:03 -0500

    OS Folders Updated

---
+++ Ubuntu/cron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# All reoccuring automated jobs for an Ubuntu System go here
+##############
+##############
+# Create cronjob to update sytem everyday and 
+# logs it in a file in the root home folder
+touch ~/.update_log.txt
+(crontab -l ; echo "0 8 * * * apt-get -y update && tee -a ~/.update_log") | contab -
