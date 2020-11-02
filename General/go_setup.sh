25486e097127a36662b3d7f200c2683b7e796c99
nickmancari <nickjmancari@gmail.com>
2020-11-02 10:32:47 -0600

    added go script

---
+++ General/go_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# setup server enviroment for Golang
+#
+
+wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz &&
+	sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz &&
+	echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+&& exit
