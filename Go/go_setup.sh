8cc2565d46621bee4a33a4c3bd7f95b446e7907f
nickmancari <nickjmancari@gmail.com>
2021-01-19 09:52:31 -0600

    added vim plugin

--- Go/go_setup.sh
+++ Go/go_setup.sh
@@ -7,5 +7,8 @@ wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz &&
 	echo "You just downloaded Go.13"
 	sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz &&
 	echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
-	go run $PWD/.test_go.go
+	go run $PWD/.test_go.go &&
+echo "Setting up VIM plugin"
+	git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+echo "VIM Plugin is setup! You GO!"
 && exit
