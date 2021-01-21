df1cc7bdf35ddc666379e0294b2aea3d7fcff3d1
nickmancari <nickjmancari@gmail.com>
2021-01-21 13:19:33 -0600

    color fix

--- General/aliases.sh
+++ General/aliases.sh
@@ -2,17 +2,17 @@
 ####
 ##
 #
+RED='\033[0;31m'
+NC='\033[0m'
+
 printf "alias 'xx=clear'" >> ~/.bashrc &&
 	printf "\nfunction cds() {\ncd \$1 ; ls\n}" >> ~/.bashrc &&
 		printf "\nfunction cdx() {\nclear ; cd \$1 ; ls\n}" >> ~/.bashrc &&
 	source ~/.bashrc
 
-echo "{RED}##############aliases are setup#################{NC}"
+echo -e "{RED}##############aliases are setup#################{NC}"
 echo "!!!You have 3 new commands that will make your life easier: xx, cds, cdx!!!"
 printf "\n"
 echo "- <xx> is an alias for clear"
 echo "- <cds> puts you in a directory and then lists the content of that directory in one stroke"
 echo "- <cdx> does the same thing that cds does but it clears the screen before is runs cds features"
-
-RED='\033[0;31m'
-NC='\033[0m'
