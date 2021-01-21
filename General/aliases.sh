a7814d4c55a0f1c792c4d417a3214823be383f7c
nickmancari <nickjmancari@gmail.com>
2021-01-21 12:02:52 -0600

    fixed literal issue

--- General/aliases.sh
+++ General/aliases.sh
@@ -3,8 +3,8 @@
 ##
 #
 printf "alias 'xx=clear'" >> ~/.bashrc &&
-	printf "\nfunction cds() {\ncd $1 ; ls\n}" >> ~/.bashrc &&
-		printf "\nfunction cdx() {\nclear ; cd $1 ; ls\n}" >> ~/.bashrc &&
+	printf "\nfunction cds() {\ncd \$1 ; ls\n}" >> ~/.bashrc &&
+		printf "\nfunction cdx() {\nclear ; cd \$1 ; ls\n}" >> ~/.bashrc &&
 	source ~/.bashrc
 
 echo "##############aliases are setup#################"
