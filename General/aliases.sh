68481b1ff8ad8b6252380dab94bf0ab749a2c4f1
nickmancari <nickjmancari@gmail.com>
2021-01-21 13:06:51 -0600

    fixed blank space issue

--- General/aliases.sh
+++ General/aliases.sh
@@ -9,7 +9,7 @@ printf "alias 'xx=clear'" >> ~/.bashrc &&
 
 echo "##############aliases are setup#################"
 echo "!!!You have 3 new commands that will make your life easier: xx, cds, cdx!!!"
-echo \n
+printf "\n"
 echo "- <xx> is an alias for clear"
 echo "- <cds> puts you in a directory and then lists the content of that directory in one stroke"
 echo "- <cdx> does the same thing that cds does but it clears the screen before is runs cds features"
