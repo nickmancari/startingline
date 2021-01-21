89b9664ce91185f1789c4c8bcfbecc8b08905807
nickmancari <nickjmancari@gmail.com>
2021-01-21 13:13:14 -0600

    add color to echo

--- General/aliases.sh
+++ General/aliases.sh
@@ -7,10 +7,12 @@ printf "alias 'xx=clear'" >> ~/.bashrc &&
 		printf "\nfunction cdx() {\nclear ; cd \$1 ; ls\n}" >> ~/.bashrc &&
 	source ~/.bashrc
 
-echo "##############aliases are setup#################"
+echo "{RED}##############aliases are setup#################{NC}"
 echo "!!!You have 3 new commands that will make your life easier: xx, cds, cdx!!!"
 printf "\n"
 echo "- <xx> is an alias for clear"
 echo "- <cds> puts you in a directory and then lists the content of that directory in one stroke"
 echo "- <cdx> does the same thing that cds does but it clears the screen before is runs cds features"
 
+RED='\033[0;31m'
+NC='\033[0m'
