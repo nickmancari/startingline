25940349db7d36cb88bc4aada0745d519480c1d5
nickmancari <nickjmancari@gmail.com>
2021-01-21 11:52:35 -0600

    more changes to alias automation

--- General/aliases.sh
+++ General/aliases.sh
@@ -5,6 +5,12 @@
 printf "alias 'xx=clear'" >> ~/.bashrc &&
 	printf "\nfunction cds() {\ncd $1 ; ls\n}" >> ~/.bashrc &&
 		printf "\nfunction cdx() {\nclear ; cd $1 ; ls\n}" >> ~/.bashrc &&
-	
-	echo "aliases are setup"
+	source ~/.bashrc
+
+echo "##############aliases are setup#################"
+echo "!!!You have 3 new commands that will make your life easier: xx, cds, cdx!!!"
+echo \n
+echo "- <xx> is an alias for clear"
+echo "- <cds> puts you in a directory and then lists the content of that directory in one stroke"
+echo "- <cdx> does the same thing that cds does but it clears the screen before is runs cds features"
 
