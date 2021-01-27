54bb8d0020e3bbe98f84aff1a936109c44e20007
nickmancari <nickjmancari@gmail.com>
2021-01-27 13:47:49 -0600

    fixed go setup issue

--- Go/go_setup.sh
+++ Go/go_setup.sh
@@ -9,10 +9,10 @@ wget -q --spider google.com
 	fi
 
 wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz &&
-	echo "You just downloaded Go.13"
+	echo -e "\e[34mYou just downloaded Go.13\e[39m"
 	sudo tar -C /usr/local -xzf go1.13.linux-amd64.tar.gz &&
 	echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile && source /etc/profile &&
-	go run $PWD/.test_go.go &&
+	go run $PWD/test_go.go &&
 echo "Setting up VIM plugin"
 	git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-echo "VIM Plugin is setup! You GO!"
+echo -e "VIM Plugin is setup! You \e[34mGO\e39m!"
