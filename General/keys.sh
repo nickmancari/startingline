71001c3fee32d6b9d1ccd5299da88efafe613004
nickmancari <nickjmancari@gmail.com>
2021-02-02 16:34:41 -0600

    fixes

--- General/keys.sh
+++ General/keys.sh
@@ -5,24 +5,23 @@ PUSH="               "
 ####
 #####
 
-function keyfunc() {
-	echo what would you like to name the key?
-	read KEYNAME
-	ssh-keygen -f ~/.ssh/$KEYNAME
-	echo Where is this going?
-	read SERVER
-	echo With what user?
-	read USER
-	ssh-copy-id -i ~/.ssh/$KEYNAME $USER@$SERVER
-}
 
 echo -e "${PUSH}##########LETS MAKE SSH KEYS##########"
 echo "Do you want make an SSH key? [y/n] [press enter]"
 read -s YESORNO
 	if [[ $YESORNO == n ]]; then
-		return
-	else keyfunc()
+		exit 1
+	else :
 	fi
+
+echo what would you like to name the key?
+read KEYNAME
+ssh-keygen -f ~/.ssh/$KEYNAME
+echo Where is this going?
+read SERVER
+echo With what user?
+read USER
+ssh-copy-id -i ~/.ssh/$KEYNAME $USER@$SERVER
 #####
 ####
 ###
