a3d4a40eddbc37468edbdf0802c15f2400d7a1c3
nickmancari <nickjmancari@gmail.com>
2021-02-10 10:52:17 -0600

    removed exit status numbers

--- Go/go_setup.sh
+++ Go/go_setup.sh
@@ -13,7 +13,7 @@ PUSH="                         "
 
 whoami | grep 'root' &> /dev/null
 	if [[ $? != 0 ]]; then
-		echo -e "${RED}Error${NC}: root required" && return 1
+		echo -e "${RED}Error${NC}: root required" && return
 	else 
 		:
 	fi 
@@ -27,7 +27,7 @@ find $PWD/Go/go_setup.sh &> /dev/null
 	
 wget -q --spider google.com
 	if [[ $? != 0 ]] ; then
-		echo -e "${RED}Error${NC}: wget not installed" && return 1
+		echo -e "${RED}Error${NC}: wget not installed" && return
 	else 
 		:
 	fi
